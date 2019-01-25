97dd803a554a4dafd7761361421f0ffb10f89b8e
kraberstein <46929793+kraberstein@users.noreply.github.com>
2019-01-25 17:46:45 +0300

    add binary_integer function

--- coursera.cpp
+++ coursera.cpp
@@ -1,26 +1,31 @@
-//============================================================================
-// Name        : coursera.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
-
 #include <iostream>
-#include <string>
+#include <vector>
 using namespace std;
 
-const int& nod(const int& a, const int& b) {
+const int& gcd(const int& a, const int& b) {	//	*** *** *** *** calculate greatest common divisor (gcd)
 	if (a == 0 || (a == 0 && b == 0))
 		return b;
 	if (b == 0)
 		return a;
-	return nod(b, a%b);
+	return gcd(b, a%b);
+}
+
+void binary_integer(int number) {	//	*** *** *** *** convert integer to binary
+	cout << number << " in binary code = ";
+	vector<int> bin_int;
+	while (number > 0) {
+		bin_int.push_back(number%2);
+		number /= 2;
+	}
+	for (int i = bin_int.size() - 1; i >= 0; --i) {
+		cout << bin_int[i];
+	}
+	cout << endl;
 }
 
 int main() {
-	int a, b;
-	cin >> a >> b;
-	cout << nod(a, b);
+	binary_integer(5);
+	gcd(52, 25);
+
 	return 0;
 }
